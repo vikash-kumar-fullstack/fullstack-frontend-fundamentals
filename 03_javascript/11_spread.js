2731731e4746e27bc9b15c51a4391746eac924bb
Vikash Kumar <nisant54321@gmail.com>
2026-03-21 14:42:07 +0530

    understanding the rest operator in js

--- 03_javascript/11_spread.js
+++ 03_javascript/11_spread.js
@@ -53,3 +53,41 @@ let finalUser = { ...user, ...details };
 console.log(finalUser);
 
 
+
+/*
+------------------------------------------
+2. REST OPERATOR (...)
+------------------------------------------
+
+Rest = "collect multiple values into one"
+Used in function parameters
+*/
+
+
+function sumAll(...numbers) {
+    // numbers becomes array
+    let sum = 0;
+
+    sum=numbers.reduce((a,b)=>a+b)
+
+    return sum;
+}
+
+console.log(sumAll(1, 2, 3));       // 6
+console.log(sumAll(10, 20, 30, 40)); // 100
+
+
+
+/*
+Rest with normal parameters
+*/
+
+function printData(a, b, ...rest) {
+    console.log("a:", a);
+    console.log("b:", b);
+    console.log("rest:", rest);
+}
+
+printData(1, 2, 3, 4, 5);
+printData(1,2)
+
