0b363b324ee68b1b929755e42bfe34b03093b1d1
Vikash Kumar <nisant54321@gmail.com>
2026-04-04 11:18:51 +0530

    now exploring the stram,buffer concept practically

--- 06_node/06_fs_module/02_practical.js
+++ 06_node/06_fs_module/02_practical.js
@@ -36,3 +36,57 @@ fs.rename("demo.txt","updated_demo.txt",(err)=>{
  if(err) throw err
  console.log("File renamed")
 })
+
+
+// ------------------------------------------------
+// 5 Stream Reading
+// ------------------------------------------------
+console.log("\nReading file using stream")
+
+const readStream = fs.createReadStream("updated_demo.txt","utf8")
+
+readStream.on("data",(chunk)=>{
+ console.log("Chunk:",chunk)
+})
+
+readStream.on("end",()=>{
+ console.log("Finished reading file")
+})
+
+
+// ------------------------------------------------
+// 6 Stream Writing
+// ------------------------------------------------
+console.log("\nWriting file using stream")
+
+const writeStream = fs.createWriteStream("stream_output.txt")
+
+writeStream.write("Stream writing line 1\n")
+writeStream.write("Stream writing line 2\n")
+
+writeStream.end()
+
+console.log("Stream writing completed")
+
+
+// ------------------------------------------------
+// 7 Buffer Example
+// ------------------------------------------------
+console.log("\nBuffer Example")
+
+const buffer = Buffer.from("Hello Node Buffer")
+
+console.log(buffer)
+
+console.log("Buffer to String:",buffer.toString())
+
+
+// ------------------------------------------------
+// 8 Delete File
+// ------------------------------------------------
+setTimeout(()=>{
+ fs.unlink("updated_demo.txt",(err)=>{
+  if(err) throw err
+  console.log("\nFile deleted")
+ })
+},3000)
